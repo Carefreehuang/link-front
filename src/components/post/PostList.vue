12e05f22fbb05ced4843a8f51157e90dd0c4f520
Carefreehuang <2380520989@qq.com>
2025-04-17 17:02:55 +0800

    fix:优化post列表展示

--- src/components/post/PostList.vue
+++ src/components/post/PostList.vue
@@ -10,63 +10,68 @@
       <template #default>
         <table class="post-table">
           <tbody class="post-tbody">
-          <tr v-for="post of props.posts" :key="post.id" class="posts">
-            <td>
-              <router-link :to="{path: '/u/' + post.author}">
-                <el-avatar class="post-author-avatar" :src="post.avatar" fit="fill"/>
-              </router-link>
-            </td>
-            <td class="post-title">
-              <router-link :to="{name: 'viewPost', params: {pid: post.id}}" v-text="post.title" class="post-title-text"/>
-            </td>
-            <td v-if="post.nickname !== null" class="post-author hidden-xs-only">
-              <router-link :to="{path: '/u/' + post.author}" v-text="post.nickname"/>
-            </td>
-            <td v-else class="post-author hidden-xs-only">
-              <router-link :to="{path: '/u/' + post.author}" v-text="post.author"/>
-            </td>
-            <td class="view-and-reply">
-              <el-tooltip content="回复数" placement="top" :show-after="600">
-                <span class="reply">
-                  <router-link :to="{path: '/p/' + post.id, hash: '#comments'}" v-text="post.replyNumber"/>
-                </span>
+            <tr v-for="post of props.posts" :key="post.id" class="posts">
+              <td>
+                <router-link :to="{ path: '/u/' + post.author }">
+                  <el-avatar class="post-author-avatar" :src="post.avatar" fit="fill" />
+                </router-link>
+              </td>
+              <td class="post-title">
+                <router-link :to="{ name: 'viewPost', params: { pid: post.id } }" v-text="post.title"
+                  class="post-title-text" />
+              </td>
+              <td v-if="post.nickname !== null" class="post-author hidden-xs-only">
+                <router-link :to="{ path: '/u/' + post.author }" v-text="post.nickname" />
+              </td>
+              <td v-else class="post-author hidden-xs-only">
+                <router-link :to="{ path: '/u/' + post.author }" v-text="post.author" />
+              </td>
+              <td class="view-and-reply">
+                <el-tooltip content="点赞量" placement="top" :show-after="600">
+                  <span class="like hidden-xs-only">
+                    <i class="czs-heart-l">&nbsp;</i>
+                    <router-link :to="{ path: '/p/' + post.id }" v-text="post.likeNumber" />
+                  </span>
+                </el-tooltip>
+                <el-tooltip content="回复数" placement="top" :show-after="600">
+                  <span class="reply">
+                    <i class="czs-talk-l">&nbsp;</i>
+                    <router-link :to="{ path: '/p/' + post.id, hash: '#comments' }" v-text="post.replyNumber" />
+                  </span>
+                </el-tooltip>
+                <el-tooltip content="点击量" placement="top" :show-after="600">
+                  <span class="view hidden-xs-only">
+                    <i class="czs-eye-l">&nbsp;</i>
+                    <router-link :to="{ path: '/p/' + post.id }" v-text="post.viewNumber" />
+                  </span>
+                </el-tooltip>
+              </td>
+              <el-tooltip v-if="props.isAllPost" :content="post.lastCommentTime" placement="top" :show-after="600">
+                <td class="post-time hidden-xs-only" v-text="moment(post.lastCommentTime).fromNow()" />
               </el-tooltip>
-              <el-tooltip content="点击量" placement="top" :show-after="600">
-                <span class="view hidden-xs-only">
-                  <router-link :to="{path: '/p/' + post.id}" v-text="post.viewNumber"/>
-                </span>
+              <el-tooltip v-else :content="post.createTime" placement="right" :show-after="600">
+                <td class="post-time hidden-xs-only" v-text="moment(post.createTime).fromNow()" />
               </el-tooltip>
-            </td>
-            <el-tooltip v-if="props.isAllPost" :content="post.lastCommentTime" placement="top" :show-after="600">
-              <td class="post-time hidden-xs-only" v-text="moment(post.lastCommentTime).fromNow()"/>
-            </el-tooltip>
-            <el-tooltip v-else :content="post.createTime" placement="right" :show-after="600">
-              <td class="post-time hidden-xs-only" v-text="moment(post.createTime).fromNow()"/>
-            </el-tooltip>
-          </tr>
+            </tr>
           </tbody>
         </table>
 
         <el-pagination class="pagination"
-                       v-if="!props.loading && !props.loadingError && props.totalItems !== undefined && props.totalItems > 0"
-                       layout="prev, pager, jumper, next, ->, total"
-                       small background
-                       :total="props.totalItems"
-                       :page-count="props.totalPage"
-                       :pager-count="5"
-                       :current-page="props.currentPage"
-                       @update:current-page="changeCurrentPage"/>
+          v-if="!props.loading && !props.loadingError && props.totalItems !== undefined && props.totalItems > 0"
+          layout="prev, pager, jumper, next, ->, total" small background :total="props.totalItems"
+          :page-count="props.totalPage" :pager-count="5" :current-page="props.currentPage"
+          @update:current-page="changeCurrentPage" />
 
-        <el-empty description="还没有帖子" v-if="props.posts && props.posts.length === 0"/>
-        <el-empty description="没有更多了" v-if="props.posts && props.posts.length < 20 && props.posts.length > 0"/>
+        <el-empty description="还没有帖子" v-if="props.posts && props.posts.length === 0" />
+        <el-empty description="没有更多了" v-if="props.posts && props.posts.length < 20 && props.posts.length > 0" />
       </template>
     </el-skeleton>
   </div>
 </template>
 
 <script setup>
-import {useRoute, useRouter} from "vue-router/dist/vue-router";
-import {ref} from "vue";
+import { useRoute, useRouter } from "vue-router/dist/vue-router";
+import { ref } from "vue";
 import moment from "moment";
 
 const router = useRouter();
@@ -104,11 +109,13 @@ function changeCurrentPage(page) {
   vertical-align: middle;
   margin-right: 8px;
 }
+
 .post-avatar-skeleton {
   width: 33px;
   height: 33px;
   margin: 5px 10px 5px 0;
 }
+
 .post-title-skeleton {
   margin-top: 13px;
   height: 18px;
@@ -116,7 +123,8 @@ function changeCurrentPage(page) {
 
 .post-title-text {
   font-size: 0.9rem;
-  color: var(--custom-trend-tr-color);;
+  color: var(--custom-trend-tr-color);
+  ;
   text-decoration: none;
 }
 
@@ -125,7 +133,7 @@ function changeCurrentPage(page) {
 }
 
 .post-title {
-  width: 74%;
+  width: 66%;
   padding-right: 8px;
 }
 
@@ -134,37 +142,62 @@ function changeCurrentPage(page) {
   font-size: 0.75rem;
   text-align: center;
 }
-.post-author > a {
+
+.post-author>a {
   color: var(--custom-trend-tr-color);
   text-decoration: unset;
 }
 
 .view-and-reply {
-  width: 8%;
+  width: 16%;
   text-align: center;
 }
 
 .reply {
   display: inline-block;
   padding: 0 3px 0 6px;
-  border-radius: 10px 0 0 6px;
-  font-size: 11px;
-  background: var(--custom-reply-num-bg-color);
+  /* border-radius: 10px 0 0 6px; */
+  font-size: 12px;
+  /* background: var(--custom-reply-num-bg-color); */
+  background: rgb(238, 238, 238);
+  border-radius: 3px;
   margin-bottom: 5px;
+  margin: 1px;
 }
-.reply > a {
-  color: var(--custom-reply-num-color);
+
+.reply>a {
+  color: var(--custom-view-num-color);
   text-decoration: unset;
 }
 
 .view {
   display: inline-block;
   padding: 0 6px 0 3px;
-  border-radius: 0 6px 10px 0;
-  font-size: 11px;
-  background: var(--custom-view-num-bg-color);
+  /* border-radius: 0 6px 10px 0; */
+  font-size: 12px;
+  /* background: var(--custom-view-num-bg-color); */
+  background: rgb(238, 238, 238);
+  border-radius: 3px;
+  margin: 1px;
 }
-.view > a {
+
+.view>a {
+  color: var(--custom-view-num-color);
+  text-decoration: unset;
+}
+
+.like {
+  display: inline-block;
+  padding: 0 6px 0 3px;
+  /* border-radius: 0 6px 10px 0; */
+  font-size: 12px;
+  /* background: var(--custom-view-num-bg-color); */
+  border-radius: 3px;
+  background: rgb(238, 238, 238);
+  margin: 1px;
+}
+
+.like>a {
   color: var(--custom-view-num-color);
   text-decoration: unset;
 }
@@ -178,31 +211,39 @@ function changeCurrentPage(page) {
     width: 24px;
     height: 24px;
   }
+
   .post-title {
     width: 88%;
   }
+
   .view-and-reply {
     width: 10%;
   }
+
   .view-and-reply {
     text-align: right;
   }
+
   .reply {
     padding: 0 6px;
     border-radius: 3px;
   }
+
   .post-avatar-skeleton {
     width: 24px;
     height: 24px;
     margin: 5px 10px 5px 0;
   }
+
   .post-title-skeleton {
     margin-top: 9px;
     height: 16px;
   }
+
   .posts-skeleton {
     padding: 5px 0;
   }
+
   .posts {
     height: 50px;
   }
@@ -228,6 +269,7 @@ td {
 tr:hover {
   background-color: var(--el-bg-color);
 }
+
 html.dark tr:hover {
   background-color: var(--el-bg-color-overlay) !important;
 }
@@ -241,18 +283,24 @@ html.dark tr:hover {
 .pagination .el-pagination__jump {
   margin-left: unset !important;
 }
+
 .pagination .el-pagination__total {
   margin-right: unset !important;
 }
+
 @media screen and (max-width: 767px) {
-  .pagination .el-pagination__jump, .pagination .el-pagination__total {
+
+  .pagination .el-pagination__jump,
+  .pagination .el-pagination__total {
     display: none !important;
   }
 }
 
-.pagination .el-pagination__goto, .pagination .el-pagination__classifier {
+.pagination .el-pagination__goto,
+.pagination .el-pagination__classifier {
   display: none !important;
 }
+
 .pagination .el-input__wrapper {
   transition: unset;
 }
